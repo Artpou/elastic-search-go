17720b728c8318e6179b74cec02f76f4cd5b020a
ATN <cedric.pierre-auguste@outlook.com>
2021-07-19 21:01:26 +0200

    require elastic search

--- app/main.go
+++ app/main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/elastic/go-elasticsearch/v7"
 )
 
 func main() {
 	fmt.Println("Hello")
+	es, _ := elasticsearch.NewDefaultClient()
+	log.Println(elasticsearch.Version)
+	log.Println(es.Info())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi")
 	})
