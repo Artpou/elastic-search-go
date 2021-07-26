39a07a0b5abec2db316e633121d361f5b1c08ff6
ATN <cedric.pierre-auguste@outlook.com>
2021-07-26 19:00:04 +0200

    add router

--- app/main.go
+++ app/main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/elastic/go-elasticsearch/v7"
-)
-
 func main() {
-	fmt.Println("Hello")
-	es, _ := elasticsearch.NewDefaultClient()
-	log.Println(elasticsearch.Version)
-	log.Println(es.Info())
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router.HandleRequests()
 }
